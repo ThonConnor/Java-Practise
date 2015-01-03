af4fe7553599b9e9dbe5486a1110b5266b84fc30
ThonConnor <setha.sethon@gmail.com>
2015-01-03 20:56:23 +0700

    added java file

---
+++ src/StringMethod/StringMethodPractise.java
@@ -0,0 +1,13 @@
+package StringMethod;
+
+public class StringMethodPractise {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
